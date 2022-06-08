f87ffff96209116305d03da64b7653655a11ed0b
Jepsuli <hanninen.jerejohannes@gmail.com>
2022-06-08 11:14:44 +0300

    kiitos teille

---
+++ Shopping/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace Shopping
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int x = 10000;
+
+            while (x > 100)
+            {
+
+
+                String path = @"C:\Users\Jere\source\repos\Shopping\Shoppinglist.txt";
+                Console.WriteLine("Anna tuote:  ");
+                string tuote = Console.ReadLine();
+                File.AppendAllText(path, tuote + Environment.NewLine);
+                String line;
+
+                try
+                {
+
+                    StreamReader sr = new StreamReader(@"C:\Users\Jere\source\repos\Shopping\Shoppinglist.txt");
+
+                    line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+
+                        Console.WriteLine(line);
+
+                        line = sr.ReadLine();
+                    }
+
+                    sr.Close();
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+            }
+        }
+    }
+}
